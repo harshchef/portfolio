ba1daffe04e046e309151a9fc487c0a7f355a70a
Aditya <adityakumar071@gmail.com>
2022-06-20 23:30:21 +0530

    2nd comit

--- portfolio/lib/screens/home_screen.dart
+++ portfolio/lib/screens/home_screen.dart
@@ -17,59 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
     double widthscreen = MediaQuery.of(context).size.width;
     double heightscreen = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              //  begin: Alignment.topLeft,
-              //end: Alignment.bottomRight,
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              stops: [
-                0.1,
-                0.4,
-                0.6,
-                0.9,
-              ],
-              colors: [secondaryColor, primaryColor, darkColor, bgColor],
-            ),
-          ),
-        ),
-        toolbarHeight: 65,
-        backgroundColor: bgColor,
-        title: Center(
-          child: Row(
-            //  mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                width: widthscreen * 0.10,
-              ),
-              RoundedButtonWidget(buttonText: "</>Aditya Kumar"),
-              SizedBox(
-                width: widthscreen * 0.45,
-              ),
-              RoundedButtonWidget(buttonText: "Home"),
-              SizedBox(
-                width: 10,
-              ),
-              RoundedButtonWidget(buttonText: "About"),
-              SizedBox(
-                width: 10,
-              ),
-              RoundedButtonWidget(buttonText: "Work"),
-              SizedBox(
-                width: 10,
-              ),
-              RoundedButtonWidget(buttonText: "Contact"),
-            ],
-          ),
-        ),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(25.0),
-                bottomRight: Radius.circular(25.0))),
-        elevation: 8,
-      ),
       body: Center(
         child: Container(
           decoration: BoxDecoration(
@@ -85,29 +32,54 @@ class _HomeScreenState extends State<HomeScreen> {
               colors: [secondaryColor, primaryColor, darkColor, bgColor],
             ),
           ),
+          constraints:
+              BoxConstraints(maxWidth: widthscreen, maxHeight: heightscreen),
           child: Center(
-            child: Text(
-              'Hello Gradient!',
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+              child: Column(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                child: NavBAr(),
               ),
-            ),
-          ),
+            ],
+          )),
         ),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class NavBAr extends StatelessWidget {
+  const NavBAr({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Gradient Example'),
-      ),
+    double widthscreen = MediaQuery.of(context).size.width;
+    double heightscreen = MediaQuery.of(context).size.height;
+    return Row(
+      //  mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: widthscreen * 0.10,
+        ),
+        RoundedButtonWidget(buttonText: "</>Aditya Kumar"),
+        SizedBox(
+          width: widthscreen * 0.45,
+        ),
+        RoundedButtonWidget(buttonText: "Home"),
+        SizedBox(
+          width: 10,
+        ),
+        RoundedButtonWidget(buttonText: "About"),
+        SizedBox(
+          width: 10,
+        ),
+        RoundedButtonWidget(buttonText: "Work"),
+        SizedBox(
+          width: 10,
+        ),
+        RoundedButtonWidget(buttonText: "Contact"),
+      ],
     );
   }
 }
